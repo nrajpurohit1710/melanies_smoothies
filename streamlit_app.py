16359b1b31ae2901bb4b4ba1c6543a025f2b208b
nrajpurohit1710 <nrajpurohit1710@gmail.com>
2026-06-12 12:31:27 +0530

    Update streamlit_app.py
    Update the API to accept dynamic input

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        sf_dt=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -41,11 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅" )
-    
-    #if ingredients_string:
-        #session.sql(my_insert_stmt).collect()
-
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_dt=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
